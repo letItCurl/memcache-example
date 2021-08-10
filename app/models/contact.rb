959fd2226cd47ad7ee29871cca8752f47ea6e320
letItCurl <rolandlopez.developer@gmail.com>
2021-08-10 11:10:48 +0700

    ✨ Expire cache.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,13 @@
 class Contact < ApplicationRecord
+  after_save    :expire_contact_all_cache
+  after_destroy :expire_contact_all_cache
+
+  def self.all_cached
+    Rails.cache.fetch('Contact.all') { all.to_a }
+  end
+
+  private
+    def expire_contact_all_cache
+      Rails.cache.delete('Contact.all')
+    end
 end
